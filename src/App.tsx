19bff6ae52c632a315c2158a7d79509f4ed838d6
Gbemi <kadri.oluwagbemi@gmail.com>
2024-12-19 05:34:33 +0200

    Add additional UI packages and default styling setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,132 @@
-import { useState } from 'react'
-import './App.css'
+'use client'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useState } from 'react'
+import { Dialog, DialogPanel, Button } from '@headlessui/react'
+import { Bars3Icon, PlusIcon, XMarkIcon } from '@heroicons/react/24/solid'
+import Logo from './assets/logo.svg'
+
+const navigation = [
+  { name: 'Influencers', href: '#' },
+  { name: 'Employees', href: '#' },
+]
+
+export default function App() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <>
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='bg-custom-gradient'>
+      <header className='absolute inset-x-0 top-0 z-50'>
+        <nav aria-label='Global' className='flex items-center justify-between p-6 lg:px-8'>
+          <div className='flex lg:flex-1'>
+            <a href='#' className='-m-1.5 p-1.5'>
+              <span className='sr-only'>Your Company</span>
+              <img alt='Logo' src={Logo} className='h-8 w-auto text-secondary' />
+            </a>
+          </div>
+          <div className='flex lg:hidden'>
+            <button
+              type='button'
+              onClick={() => setMobileMenuOpen(true)}
+              className='-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-secondary'
+            >
+              <span className='sr-only'>Open main menu</span>
+              <Bars3Icon aria-hidden='true' className='size-6' />
+            </button>
+          </div>
+          <div className='hidden lg:flex lg:gap-x-12'>
+            {navigation.map((item) => (
+              <a key={item.name} href={item.href} className='text-sm/6 font-semibold text-secondary'>
+                {item.name}
+              </a>
+            ))}
+          </div>
+          <div className='hidden lg:flex lg:flex-1 lg:justify-end'>
+            <a href='#' className='text-sm/6 font-semibold text-secondary'>
+              Log in <span aria-hidden='true'>&rarr;</span>
+            </a>
+          </div>
+        </nav>
+        <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className='lg:hidden'>
+          <div className='fixed inset-0 z-50' />
+          <DialogPanel className='fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-secondary/10'>
+            <div className='flex items-center justify-between'>
+              <a href='#' className='-m-1.5 p-1.5'>
+                <span className='sr-only'>Your Company</span>
+                <img
+                  alt=''
+                  src='https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600'
+                  className='h-8 w-auto'
+                />
+              </a>
+              <button
+                type='button'
+                onClick={() => setMobileMenuOpen(false)}
+                className='-m-2.5 rounded-md p-2.5 text-secondary'
+              >
+                <span className='sr-only'>Close menu</span>
+                <XMarkIcon aria-hidden='true' className='size-6' />
+              </button>
+            </div>
+            <div className='mt-6 flow-root'>
+              <div className='-my-6 divide-y divide-gray-500/10'>
+                <div className='space-y-2 py-6'>
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className='-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-secondary hover:bg-gray-50'
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <div className='py-6'>
+                  <a
+                    href='#'
+                    className='-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-secondary hover:bg-gray-50'
+                  >
+                    Log in
+                  </a>
+                </div>
+              </div>
+            </div>
+          </DialogPanel>
+        </Dialog>
+      </header>
+
+      <main className='pt-28 px-10 grid gap-8'>
+        <div className='flex justify-end'>
+          <Button className='inline-flex items-center gap-1 rounded-md bg-primary py-3 px-3 text-sm/6 font-semibold text-dark shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[open]:bg-secondary data-[focus]:outline-1 data-[focus]:outline-white'>
+            <PlusIcon className='h-5 w-5'/> <span>Create new influencer</span>
+          </Button>
+        </div>
+        <table className='table-fixed text-sm rounded-xl text-left rtl:text-right xl:w-5/6 2xl:w-10/12 mx-auto shadow-lg'>
+          <thead className='text-xs text-secondary uppercase border-b border-primary/10'>
+            <tr>
+              <th>Song</th>
+              <th>Artist</th>
+              <th>Year</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+              <td>Malcolm Lockyer</td>
+              <td>1961</td>
+            </tr>
+            <tr>
+              <td>Witchy Woman</td>
+              <td>The Eagles</td>
+              <td>1972</td>
+            </tr>
+            <tr>
+              <td>Shining Star</td>
+              <td>Earth, Wind, and Fire</td>
+              <td>1975</td>
+            </tr>
+          </tbody>
+        </table>
+      </main>
+    </div>
   )
 }
-
-export default App
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#e9f02e',
+        secondary: '#161616',
+      },
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(180deg, #ccc, #ededed00)',
+      },
+      backgroundColor: {
+        primary: '#e9f02e',
+        secondary: '#9da093',
+      },
+    },
+  },
+  plugins: [],
+}
