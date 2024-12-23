600a51054115c0d54a9659b16d09b510ef68dc8b
Gbemi <kadri.oluwagbemi@gmail.com>
2024-12-23 16:05:15 +0200

    Update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,17 @@
-import { defineConfig, loadEnv  } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL, 
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-          secure: false,
-          ws: true,
-        },
-      },
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://management-app.fly.dev/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     },
-  };
-});
+  },
+
+})
