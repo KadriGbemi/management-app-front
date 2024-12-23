1f70c5128813e663c44142fdef05c6ccbc023422
Gbemi <kadri.oluwagbemi@gmail.com>
2024-12-23 17:02:05 +0200

    Fix refresh in deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,30 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import react from '@vitejs/plugin-react'
+import vercel from 'vite-plugin-vercel';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        },
+// https://vite.dev/config/
+export default defineConfig({
+  define: {
+    __APP_ENV__: process.env.VITE_API_URL,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'nested/index.html'),
       },
     },
-  };
-});
+  },
+  plugins: [react(), vercel()],
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    },
+  },
+
+})
