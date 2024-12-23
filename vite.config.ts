e9eb8ce6f61281da2b5bfd43cdb2f1decd9da2e6
Gbemi <kadri.oluwagbemi@gmail.com>
2024-12-23 14:36:53 +0200

    Update vite config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv  } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5050/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    },
-  },
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the mode
+  const env = loadEnv(mode, process.cwd());
 
-})
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL, // Use the loaded environment variable here
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  };
+});
